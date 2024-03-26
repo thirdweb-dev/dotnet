dcb3d5d7b6bdae39378707682af84bba57dd9c8f
0xFirekeeper <0xFirekeeper@gmail.com>
2024-03-26 05:40:42 +0300

    ver

--- Thirdweb/Thirdweb.Utils/Constants.cs
+++ Thirdweb/Thirdweb.Utils/Constants.cs
@@ -2,7 +2,7 @@
 {
     public static class Constants
     {
-        internal const string Version = "0.0.0";
+        internal const string Version = "0.0.1";
         internal const int DefaultFetchTimeout = 60000;
     }
 }
