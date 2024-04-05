8fb4114a2092b918fc8207689596cbd7211b0c7a
Firekeeper <0xFirekeeper@gmail.com>
2024-04-05 23:20:09 +0300

    Storage download upload (#4)
    * Storage download upload
    
    * Update dotnet-ci.yml

---
+++ Thirdweb/Thirdweb.Storage/ThirdwebStorage.cs
@@ -0,0 +1,77 @@
+using Newtonsoft.Json;
+
+namespace Thirdweb
+{
+    public class ThirdwebStorage
+    {
+        public static async Task<T> Download<T>(ThirdwebClient client, string uri, int? requestTimeout = null)
+        {
+            if (string.IsNullOrEmpty(uri))
+            {
+                throw new ArgumentNullException(nameof(uri));
+            }
+
+            uri = uri.ReplaceIPFS(string.IsNullOrEmpty(client.ClientId) ? Constants.FALLBACK_IPFS_GATEWAY : $"https://{client.ClientId}.ipfscdn.io/ipfs/");
+
+            using var httpClient = new HttpClient();
+
+            var isThirdwebRequest = new Uri(uri).Host.EndsWith(".ipfscdn.io");
+            if (isThirdwebRequest)
+            {
+                var headers = Utils.GetThirdwebHeaders(client);
+                foreach (var header in headers)
+                {
+                    httpClient.DefaultRequestHeaders.Add(header.Key, header.Value);
+                }
+            }
+
+            requestTimeout ??= client.FetchTimeoutOptions.GetTimeout(TimeoutType.Storage);
+
+            var response = await httpClient.GetAsync(uri, new CancellationTokenSource(requestTimeout.Value).Token);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Failed to download {uri}: {response.StatusCode} | {response.ReasonPhrase} | {await response.Content.ReadAsStringAsync()}");
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+
+            return typeof(T) == typeof(string) ? (T)(object)content : JsonConvert.DeserializeObject<T>(content);
+        }
+
+        public static async Task<IPFSUploadResult> Upload(ThirdwebClient client, string path)
+        {
+            if (string.IsNullOrEmpty(client.ClientId))
+            {
+                throw new UnauthorizedAccessException("You cannot use Upload features without setting a Client ID.");
+            }
+
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentNullException(nameof(path));
+            }
+
+            using var httpClient = new HttpClient();
+            using var form = new MultipartFormDataContent { { new ByteArrayContent(File.ReadAllBytes(path)), "file", Path.GetFileName(path) } };
+
+            var headers = Utils.GetThirdwebHeaders(client);
+            foreach (var header in headers)
+            {
+                httpClient.DefaultRequestHeaders.Add(header.Key, header.Value);
+            }
+
+            var response = await httpClient.PostAsync(Constants.PIN_URI, form);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Failed to upload {path}: {response.StatusCode} | {response.ReasonPhrase} | {await response.Content.ReadAsStringAsync()}");
+            }
+
+            var result = await response.Content.ReadAsStringAsync();
+
+            var res = JsonConvert.DeserializeObject<IPFSUploadResult>(result);
+            res.PreviewUrl = $"https://{client.ClientId}.ipfscdn.io/ipfs/{res.IpfsHash}";
+            return res;
+        }
+    }
+}
--- Thirdweb/Thirdweb.Utils/Utils.cs
+++ Thirdweb/Thirdweb.Utils/Utils.cs
@@ -95,5 +95,39 @@ namespace Thirdweb
         {
             return DateTimeOffset.UtcNow.ToUnixTimeSeconds() + 60 * 60 * 24 * 365 * 10;
         }
+
+        public static string ReplaceIPFS(this string uri, string gateway = null)
+        {
+            gateway ??= Constants.FALLBACK_IPFS_GATEWAY;
+            return !string.IsNullOrEmpty(uri) && uri.StartsWith("ipfs://") ? uri.Replace("ipfs://", gateway) : uri;
+        }
+
+        public static Dictionary<string, string> GetThirdwebHeaders(ThirdwebClient client)
+        {
+            var headers = new Dictionary<string, string>
+            {
+                { "x-sdk-name", "Thirdweb.NET" },
+                { "x-sdk-os", System.Runtime.InteropServices.RuntimeInformation.OSDescription },
+                { "x-sdk-platform", "dotnet" },
+                { "x-sdk-version", Constants.VERSION }
+            };
+
+            if (!string.IsNullOrEmpty(client.ClientId))
+            {
+                headers.Add("x-client-id", client.ClientId);
+            }
+
+            if (!string.IsNullOrEmpty(client.SecretKey))
+            {
+                headers.Add("x-secret-key", client.SecretKey);
+            }
+
+            if (!string.IsNullOrEmpty(client.BundleId))
+            {
+                headers.Add("x-bundle-id", client.BundleId);
+            }
+
+            return headers;
+        }
     }
 }
