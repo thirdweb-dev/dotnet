dc92366c203cae8c020ae8e8c9facd5645fe839c
0xFirekeeper <0xFirekeeper@gmail.com>
2024-06-13 11:15:36 +0300

    Native transfer extension

--- Thirdweb.Tests/Thirdweb.WalletExtension.Tests.cs
+++ Thirdweb.Tests/Thirdweb.WalletExtension.Tests.cs
@@ -13,7 +13,8 @@ namespace Thirdweb.Tests
         {
             var client = ThirdwebClient.Create(secretKey: _secretKey);
             var privateKeyWallet = await PrivateKeyWallet.Create(client, _testPrivateKey);
-            return privateKeyWallet;
+            var smartAccount = await SmartWallet.Create(client, personalWallet: privateKeyWallet, factoryAddress: "0xbf1C9aA4B1A085f7DA890a44E82B0A1289A40052", gasless: true, chainId: 421614);
+            return smartAccount;
         }
 
         [Fact]
@@ -21,15 +22,26 @@ namespace Thirdweb.Tests
         {
             var client = ThirdwebClient.Create(secretKey: _secretKey);
             var wallet = await GetWallet();
+
             _ = await Assert.ThrowsAsync<ArgumentNullException>(async () => await wallet.GetBalance(null, _chainId));
             _ = await Assert.ThrowsAsync<ArgumentOutOfRangeException>(async () => await wallet.GetBalance(client, BigInteger.Zero));
             _ = await Assert.ThrowsAsync<ArgumentOutOfRangeException>(async () => await wallet.GetBalance(client, -1));
 
+            _ = await Assert.ThrowsAsync<ArgumentNullException>(async () => await wallet.Transfer(null, BigInteger.Zero, null, BigInteger.Zero));
+            _ = await Assert.ThrowsAsync<ArgumentException>(async () => await wallet.Transfer(client, BigInteger.Zero, null, BigInteger.Zero));
+            _ = await Assert.ThrowsAsync<ArgumentException>(async () => await wallet.Transfer(client, -1, string.Empty, BigInteger.Zero));
+            _ = await Assert.ThrowsAsync<ArgumentException>(async () => await wallet.Transfer(client, _chainId, null, BigInteger.Zero));
+            _ = await Assert.ThrowsAsync<ArgumentException>(async () => await wallet.Transfer(client, _chainId, string.Empty, BigInteger.Zero));
+            _ = await Assert.ThrowsAsync<ArgumentOutOfRangeException>(async () => await wallet.Transfer(client, _chainId, Constants.ADDRESS_ZERO, -1));
+
             client = null;
+
             _ = await Assert.ThrowsAsync<ArgumentNullException>(async () => await wallet.GetBalance(client, _chainId));
+            _ = await Assert.ThrowsAsync<ArgumentNullException>(async () => await wallet.Transfer(client, BigInteger.Zero, null, BigInteger.Zero));
 
             wallet = null;
             _ = await Assert.ThrowsAsync<ArgumentNullException>(async () => await wallet.GetBalance(client, _chainId));
+            _ = await Assert.ThrowsAsync<ArgumentNullException>(async () => await wallet.Transfer(client, BigInteger.Zero, null, BigInteger.Zero));
         }
 
         [Fact]
@@ -39,6 +51,17 @@ namespace Thirdweb.Tests
             var wallet = await GetWallet();
             var balance = await wallet.GetBalance(client, _chainId);
             Assert.True(balance >= 0);
+        }
+
+        [Fact]
+        public async Task Transfer()
+        {
+            var client = ThirdwebClient.Create(secretKey: _secretKey);
+            var wallet = await GetWallet();
+            var toAddress = await wallet.GetAddress();
+            var receipt = await wallet.Transfer(client, _chainId, toAddress, BigInteger.Zero);
+            Assert.NotNull(receipt);
+            Assert.True(receipt.TransactionHash.Length == 66);
         }
     }
 }
--- Thirdweb/Thirdweb.Wallets/ThirdwebWalletExtensions.cs
+++ Thirdweb/Thirdweb.Wallets/ThirdwebWalletExtensions.cs
@@ -1,5 +1,6 @@
 using System.Numerics;
 using Nethereum.Hex.HexTypes;
+using Nethereum.RPC.Eth.DTOs;
 
 namespace Thirdweb
 {
@@ -25,6 +26,43 @@ namespace Thirdweb
             var rpc = ThirdwebRPC.GetRpcInstance(client, chainId);
             var balanceHex = await rpc.SendRequestAsync<string>("eth_getBalance", await wallet.GetAddress(), "latest");
             return new HexBigInteger(balanceHex).Value;
+        }
+
+        public static async Task<TransactionReceipt> Transfer(this IThirdwebWallet wallet, ThirdwebClient client, BigInteger chainId, string toAddress, BigInteger weiAmount)
+        {
+            if (wallet == null)
+            {
+                throw new ArgumentNullException(nameof(wallet));
+            }
+
+            if (client == null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+
+            if (string.IsNullOrEmpty(toAddress))
+            {
+                throw new ArgumentException(nameof(toAddress), "Recipient address cannot be null or empty.");
+            }
+
+            if (weiAmount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(weiAmount), "Amount must be 0 or greater.");
+            }
+
+            if (chainId <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(chainId), "Chain ID must be greater than 0.");
+            }
+
+            var txInput = new ThirdwebTransactionInput()
+            {
+                From = await wallet.GetAddress(),
+                To = toAddress,
+                Value = new HexBigInteger(weiAmount)
+            };
+            var tx = await ThirdwebTransaction.Create(client, wallet, txInput, chainId);
+            return await ThirdwebTransaction.SendAndWaitForTransactionReceipt(tx);
         }
 
         // TODO: Tx Listener?
