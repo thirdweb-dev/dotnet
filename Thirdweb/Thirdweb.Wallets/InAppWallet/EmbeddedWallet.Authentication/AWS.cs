78d0c4b4126e36a20d9f69f35f512a2d43ea0858
0xFirekeeper <0xFirekeeper@gmail.com>
2024-06-21 04:52:29 +0300

    Lambda invoking REST version // remove AWS SDK

--- Thirdweb/Thirdweb.Wallets/InAppWallet/EmbeddedWallet.Authentication/AWS.cs
+++ Thirdweb/Thirdweb.Wallets/InAppWallet/EmbeddedWallet.Authentication/AWS.cs
@@ -1,7 +1,5 @@
-﻿using System.Text;
-using Amazon.CognitoIdentity;
-using Amazon.Lambda;
-using Amazon.Lambda.Model;
+﻿using System.Security.Cryptography;
+using System.Text;
 using Newtonsoft.Json;
 
 namespace Thirdweb.EWS
@@ -121,14 +119,156 @@ namespace Thirdweb.EWS
 
         internal static async Task<MemoryStream> InvokeRecoverySharePasswordLambdaAsync(string idToken, string invokePayload)
         {
-            var region = Amazon.RegionEndpoint.USWest2;
-            var request = new InvokeRequest() { FunctionName = recoverySharePasswordLambdaFunctionName, Payload = invokePayload, };
-            var credentials = new CognitoAWSCredentials(cognitoIdentityPoolId, region);
-            var providerName = $"cognito-idp.{awsRegion}.amazonaws.com/{cognitoUserPoolId}";
-            credentials.AddLogin(providerName, idToken);
-            var client = new AmazonLambdaClient(credentials, region);
-            var lambdaResponse = await client.InvokeAsync(request).ConfigureAwait(false);
-            return lambdaResponse.Payload;
+            var credentials = await GetTemporaryCredentialsAsync(idToken);
+            return await InvokeLambdaWithTemporaryCredentialsAsync(credentials, invokePayload);
+        }
+
+        private static async Task<AwsCredentials> GetTemporaryCredentialsAsync(string idToken)
+        {
+            using var client = new HttpClient();
+            var endpoint = $"https://cognito-identity.{awsRegion}.amazonaws.com/";
+
+            var payloadForGetId = new { IdentityPoolId = cognitoIdentityPoolId, Logins = new Dictionary<string, string> { { $"cognito-idp.{awsRegion}.amazonaws.com/{cognitoUserPoolId}", idToken } } };
+
+            var content = new StringContent(JsonConvert.SerializeObject(payloadForGetId), Encoding.UTF8, "application/x-amz-json-1.1");
+
+            var request = new HttpRequestMessage(HttpMethod.Post, endpoint) { Content = content };
+
+            request.Headers.Add("X-Amz-Target", "AWSCognitoIdentityService.GetId");
+
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+            var responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Failed to get identity ID: {responseContent}");
+            }
+
+            var identityIdResponse = JsonConvert.DeserializeObject<GetIdResponse>(responseContent);
+
+            var payloadForGetCredentials = new
+            {
+                IdentityId = identityIdResponse.IdentityId,
+                Logins = new Dictionary<string, string> { { $"cognito-idp.{awsRegion}.amazonaws.com/{cognitoUserPoolId}", idToken } }
+            };
+
+            content = new StringContent(JsonConvert.SerializeObject(payloadForGetCredentials), Encoding.UTF8, "application/x-amz-json-1.1");
+
+            request = new HttpRequestMessage(HttpMethod.Post, endpoint) { Content = content };
+
+            request.Headers.Add("X-Amz-Target", "AWSCognitoIdentityService.GetCredentialsForIdentity");
+
+            response = await client.SendAsync(request).ConfigureAwait(false);
+            responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Failed to get credentials: {responseContent}");
+            }
+
+            var credentialsResponse = JsonConvert.DeserializeObject<GetCredentialsForIdentityResponse>(responseContent);
+
+            return new AwsCredentials
+            {
+                AccessKeyId = credentialsResponse.Credentials.AccessKeyId,
+                SecretAccessKey = credentialsResponse.Credentials.SecretKey,
+                SessionToken = credentialsResponse.Credentials.SessionToken
+            };
+        }
+
+        private static async Task<MemoryStream> InvokeLambdaWithTemporaryCredentialsAsync(AwsCredentials credentials, string invokePayload)
+        {
+            var endpoint = $"https://lambda.{awsRegion}.amazonaws.com/2015-03-31/functions/{recoverySharePasswordLambdaFunctionName}/invocations";
+            var requestBody = new StringContent(invokePayload, Encoding.UTF8, "application/json");
+
+            using var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Post, endpoint) { Content = requestBody };
+
+            var dateTimeNow = DateTime.UtcNow;
+            var dateStamp = dateTimeNow.ToString("yyyyMMdd");
+            var amzDate = dateTimeNow.ToString("yyyyMMddTHHmmssZ");
+
+            var canonicalUri = "/2015-03-31/functions/" + Uri.EscapeDataString(recoverySharePasswordLambdaFunctionName) + "/invocations";
+            var canonicalQueryString = "";
+            var canonicalHeaders = $"host:lambda.{awsRegion}.amazonaws.com\nx-amz-date:{amzDate}\n";
+            var signedHeaders = "host;x-amz-date";
+
+            using var sha256 = SHA256.Create();
+            var payloadHash = ToHexString(sha256.ComputeHash(Encoding.UTF8.GetBytes(invokePayload)));
+            var canonicalRequest = $"POST\n{canonicalUri}\n{canonicalQueryString}\n{canonicalHeaders}\n{signedHeaders}\n{payloadHash}";
+
+            var algorithm = "AWS4-HMAC-SHA256";
+            var credentialScope = $"{dateStamp}/{awsRegion}/lambda/aws4_request";
+            var stringToSign = $"{algorithm}\n{amzDate}\n{credentialScope}\n{ToHexString(sha256.ComputeHash(Encoding.UTF8.GetBytes(canonicalRequest)))}";
+
+            var signingKey = GetSignatureKey(credentials.SecretAccessKey, dateStamp, awsRegion, "lambda");
+            var signature = ToHexString(HMACSHA256(signingKey, stringToSign));
+
+            var authorizationHeader = $"{algorithm} Credential={credentials.AccessKeyId}/{credentialScope}, SignedHeaders={signedHeaders}, Signature={signature}";
+
+            request.Headers.Add("x-amz-date", amzDate);
+            request.Headers.TryAddWithoutValidation("Authorization", authorizationHeader);
+            request.Headers.Add("x-amz-security-token", credentials.SessionToken);
+
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+            var responseContent = await response.Content.ReadAsStringAsync().ConfigureAwait(false);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                throw new Exception($"Lambda invocation failed: {responseContent}");
+            }
+
+            var memoryStream = new MemoryStream(Encoding.UTF8.GetBytes(responseContent));
+            return memoryStream;
+        }
+
+        private static byte[] HMACSHA256(byte[] key, string data)
+        {
+            using var hmac = new HMACSHA256(key);
+            return hmac.ComputeHash(Encoding.UTF8.GetBytes(data));
+        }
+
+        private static byte[] GetSignatureKey(string key, string dateStamp, string regionName, string serviceName)
+        {
+            var kDate = HMACSHA256(Encoding.UTF8.GetBytes("AWS4" + key), dateStamp);
+            var kRegion = HMACSHA256(kDate, regionName);
+            var kService = HMACSHA256(kRegion, serviceName);
+            var kSigning = HMACSHA256(kService, "aws4_request");
+            return kSigning;
+        }
+
+        private static string ToHexString(byte[] bytes)
+        {
+            var hex = new StringBuilder(bytes.Length * 2);
+            foreach (var b in bytes)
+            {
+                hex.AppendFormat("{0:x2}", b);
+            }
+            return hex.ToString();
+        }
+
+        internal class GetIdResponse
+        {
+            public string IdentityId { get; set; }
+        }
+
+        internal class GetCredentialsForIdentityResponse
+        {
+            public Credentials Credentials { get; set; }
+        }
+
+        internal class Credentials
+        {
+            public string AccessKeyId { get; set; }
+            public string SecretKey { get; set; }
+            public string SessionToken { get; set; }
+        }
+
+        internal class AwsCredentials
+        {
+            public string AccessKeyId { get; set; }
+            public string SecretAccessKey { get; set; }
+            public string SessionToken { get; set; }
         }
 
         internal class CredentialsResponse
@@ -172,30 +312,6 @@ namespace Thirdweb.EWS
             public string AccessToken { get; }
             public string IdToken { get; }
             public string RefreshToken { get; }
-        }
-
-        internal class GetIdResponse
-        {
-            public string IdentityId { get; set; }
-        }
-
-        internal class GetCredentialsForIdentityResponse
-        {
-            public Credentials Credentials { get; set; }
-        }
-
-        internal class Credentials
-        {
-            public string AccessKeyId { get; set; }
-            public string SecretKey { get; set; }
-            public string SessionToken { get; set; }
-        }
-
-        internal class AwsCredentials
-        {
-            public string AccessKeyId { get; set; }
-            public string SecretAccessKey { get; set; }
-            public string SessionToken { get; set; }
         }
     }
 }
