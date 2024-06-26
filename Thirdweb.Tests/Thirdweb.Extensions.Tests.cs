a653777093bc4fa862056c1ed70dfcc0f7c7d483
0xFirekeeper <0xFirekeeper@gmail.com>
2024-06-27 02:40:00 +0300

    Update Thirdweb.Extensions.Tests.cs

--- Thirdweb.Tests/Thirdweb.Extensions.Tests.cs
+++ Thirdweb.Tests/Thirdweb.Extensions.Tests.cs
@@ -122,7 +122,7 @@ namespace Thirdweb.Tests
         public async Task GetBalanceRaw_WithERC20()
         {
             var address = "0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045"; // vitalik.eth
-            var chainId = BigInteger.One;
+            var chainId = _chainId;
             var contractAddress = _tokenErc20ContractAddress;
             var balance = await ThirdwebExtensions.GetBalanceRaw(_client, chainId, address, contractAddress);
             Assert.True(balance >= 0);
