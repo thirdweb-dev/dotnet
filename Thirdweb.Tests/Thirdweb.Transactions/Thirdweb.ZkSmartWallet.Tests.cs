0778d85749aae1651411beed6a44ae0b37c67979
0xFirekeeper <0xFirekeeper@gmail.com>
2024-08-27 18:28:15 +0300

    Update Thirdweb.ZkSmartWallet.Tests.cs

--- Thirdweb.Tests/Thirdweb.Transactions/Thirdweb.ZkSmartWallet.Tests.cs
+++ Thirdweb.Tests/Thirdweb.Transactions/Thirdweb.ZkSmartWallet.Tests.cs
@@ -90,22 +90,22 @@ public class ZkSmartWalletTests : BaseTests
         Assert.True(hash.Length == 66);
     }
 
-    [Fact(Timeout = 120000)]
-    public async Task SendGaslessZkTx_ZkCandy_Success()
-    {
-        var account = await this.GetSmartAccount(zkChainId: 302);
-        var hash = await account.SendTransaction(
-            new ThirdwebTransactionInput(302)
-            {
-                From = await account.GetAddress(),
-                To = await account.GetAddress(),
-                Value = new Nethereum.Hex.HexTypes.HexBigInteger(0),
-                Data = "0x"
-            }
-        );
-        Assert.NotNull(hash);
-        Assert.True(hash.Length == 66);
-    }
+    // [Fact(Timeout = 120000)]
+    // public async Task SendGaslessZkTx_ZkCandy_Success()
+    // {
+    //     var account = await this.GetSmartAccount(zkChainId: 302);
+    //     var hash = await account.SendTransaction(
+    //         new ThirdwebTransactionInput(302)
+    //         {
+    //             From = await account.GetAddress(),
+    //             To = await account.GetAddress(),
+    //             Value = new Nethereum.Hex.HexTypes.HexBigInteger(0),
+    //             Data = "0x"
+    //         }
+    //     );
+    //     Assert.NotNull(hash);
+    //     Assert.True(hash.Length == 66);
+    // }
 
     [Fact(Timeout = 120000)]
     public async Task SendGaslessZkTx_Abstract_Success()
