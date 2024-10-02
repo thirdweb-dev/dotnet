cad37b15ade2da601177b1b0fdd2788fe56b8aa2
0xFirekeeper <0xFirekeeper@gmail.com>
2024-10-02 20:04:55 +0300

    Migrate all paths to bundler v2

--- Thirdweb.Console/Program.cs
+++ Thirdweb.Console/Program.cs
@@ -37,21 +37,21 @@ var privateKeyWallet = await PrivateKeyWallet.Generate(client: client);
 
 #region AA 0.6
 
-// var smartWallet06 = await SmartWallet.Create(personalWallet: privateKeyWallet, chainId: 421614, gasless: true, entryPoint: Constants.ENTRYPOINT_ADDRESS_V06);
+var smartWallet06 = await SmartWallet.Create(personalWallet: privateKeyWallet, chainId: 421614, gasless: true, entryPoint: Constants.ENTRYPOINT_ADDRESS_V06);
 
-// var receipt06 = await smartWallet06.ExecuteTransaction(new ThirdwebTransactionInput(chainId: 421614, to: await smartWallet06.GetAddress(), value: 0, data: "0x"));
+var receipt06 = await smartWallet06.ExecuteTransaction(new ThirdwebTransactionInput(chainId: 421614, to: await smartWallet06.GetAddress(), value: 0, gas: 169000, data: "0x"));
 
-// Console.WriteLine($"Receipt: {receipt06}");
+Console.WriteLine($"Receipt: {receipt06}");
 
 #endregion
 
 #region AA 0.7
 
-// var smartWallet07 = await SmartWallet.Create(personalWallet: privateKeyWallet, chainId: 421614, gasless: true, entryPoint: Constants.ENTRYPOINT_ADDRESS_V07);
+var smartWallet07 = await SmartWallet.Create(personalWallet: privateKeyWallet, chainId: 421614, gasless: true, entryPoint: Constants.ENTRYPOINT_ADDRESS_V07);
 
-// var receipt07 = await smartWallet07.ExecuteTransaction(new ThirdwebTransactionInput(chainId: 421614, to: await smartWallet07.GetAddress(), value: 0, data: "0x"));
+var receipt07 = await smartWallet07.ExecuteTransaction(new ThirdwebTransactionInput(chainId: 421614, to: await smartWallet07.GetAddress(), value: 0, gas: 169000, data: "0x"));
 
-// Console.WriteLine($"Receipt: {receipt07}");
+Console.WriteLine($"Receipt: {receipt07}");
 
 #endregion
 
