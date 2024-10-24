bc4c326f403957a27334c67314a0422560c21487
0xFirekeeper <0xFirekeeper@gmail.com>
2024-10-24 18:04:07 +0700

    ver

--- Thirdweb/Thirdweb.Utils/Constants.cs
+++ Thirdweb/Thirdweb.Utils/Constants.cs
@@ -19,7 +19,7 @@ public static class Constants
     public const string REDIRECT_HTML =
         "<html lang=\"en\" style=\"background-color:#050505;color:#fff\"><body style=\"position:relative;display:flex;flex-direction:column;height:100%;width:100%;margin:0;justify-content:center;align-items:center;text-align:center;overflow:hidden\"><div style=\"position:fixed;top:0;left:50%;background-image:radial-gradient(ellipse at center,hsl(260deg 78% 35% / 40%),transparent 60%);width:2400px;height:1400px;transform:translate(-50%,-50%);z-index:-1\"></div><h1>Authentication Complete!</h1><h2>You may close this tab now and return to the game</h2></body></html>";
 
-    internal const string VERSION = "2.5.2";
+    internal const string VERSION = "2.6.0";
     internal const int DEFAULT_FETCH_TIMEOUT = 120000;
     internal const string DUMMY_SIG = "0xfffffffffffffffffffffffffffffff0000000000000000000000000000000007aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa1c";
     internal const string DUMMY_PAYMASTER_AND_DATA_HEX =
