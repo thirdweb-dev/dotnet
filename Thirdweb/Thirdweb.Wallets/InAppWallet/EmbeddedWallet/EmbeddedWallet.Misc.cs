c6a99a7f174b3206410b84ee2c5fbeb44858db74
0xFirekeeper <0xFirekeeper@gmail.com>
2024-11-06 20:34:47 +0700

    Update EmbeddedWallet.Misc.cs

--- Thirdweb/Thirdweb.Wallets/InAppWallet/EmbeddedWallet/EmbeddedWallet.Misc.cs
+++ Thirdweb/Thirdweb.Wallets/InAppWallet/EmbeddedWallet/EmbeddedWallet.Misc.cs
@@ -1,7 +1,5 @@
 using System.Security.Cryptography;
 using Nethereum.Web3.Accounts;
-using System;
-using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Paddings;
 using Org.BouncyCastle.Crypto.Engines;
 using Org.BouncyCastle.Crypto.Parameters;
