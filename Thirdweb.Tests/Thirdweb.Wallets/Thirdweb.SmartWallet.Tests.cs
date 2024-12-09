671ef84fe11b4b6eb4f7938922e761fa8ffb171c
Firekeeper <0xFirekeeper@gmail.com>
2024-12-09 21:34:28 +0700

    [SmartWallet] Fix SwitchNetwork from zksync stack to non zksync (#106)

--- Thirdweb.Tests/Thirdweb.Wallets/Thirdweb.SmartWallet.Tests.cs
+++ Thirdweb.Tests/Thirdweb.Wallets/Thirdweb.SmartWallet.Tests.cs
@@ -314,41 +314,103 @@ public class SmartWalletTests : BaseTests
         Assert.True(hashes[1].TransactionHash.Length == 66);
     }
 
-    // [Fact(Timeout = 120000)]
-    // public async Task MultiChainTransaction_Success()
-    // {
-    //     var chainId1 = 11155111;
-    //     var chainId2 = 421614;
+    [Fact(Timeout = 120000)]
+    public async Task MultiChainTransaction_Success()
+    {
+        var chainId1 = 11155111;
+        var chainId2 = 421614;
 
-    //     var smartWallet = await SmartWallet.Create(
-    //         personalWallet: await PrivateKeyWallet.Generate(this.Client),
-    //         chainId: chainId1,
-    //         gasless: true,
-    //         factoryAddress: Constants.DEFAULT_FACTORY_ADDRESS_V06,
-    //         entryPoint: Constants.ENTRYPOINT_ADDRESS_V06
-    //     );
+        var smartWallet = await SmartWallet.Create(personalWallet: await PrivateKeyWallet.Generate(this.Client), chainId: chainId1, gasless: true);
 
-    //     var address1 = await smartWallet.GetAddress();
-    //     var receipt1 = await smartWallet.ExecuteTransaction(new ThirdwebTransactionInput(chainId1) { To = address1, });
-    //     var nonce1 = await smartWallet.GetTransactionCount(chainId: chainId1, blocktag: "latest");
+        var address1 = await smartWallet.GetAddress();
+        var receipt1 = await smartWallet.ExecuteTransaction(new ThirdwebTransactionInput(chainId1) { To = address1, });
+        var nonce1 = await smartWallet.GetTransactionCount(chainId: chainId1, blocktag: "latest");
 
-    //     var address2 = await smartWallet.GetAddress();
-    //     var receipt2 = await smartWallet.ExecuteTransaction(new ThirdwebTransactionInput(chainId2) { To = address2, });
-    //     var nonce2 = await smartWallet.GetTransactionCount(chainId: chainId2, blocktag: "latest");
+        var address2 = await smartWallet.GetAddress();
+        var receipt2 = await smartWallet.ExecuteTransaction(new ThirdwebTransactionInput(chainId2) { To = address2, });
+        var nonce2 = await smartWallet.GetTransactionCount(chainId: chainId2, blocktag: "latest");
 
-    //     Assert.NotNull(address1);
-    //     Assert.NotNull(address2);
-    //     Assert.Equal(address1, address2);
+        Assert.NotNull(address1);
+        Assert.NotNull(address2);
+        Assert.Equal(address1, address2);
 
-    //     Assert.NotNull(receipt1);
-    //     Assert.NotNull(receipt2);
+        Assert.NotNull(receipt1);
+        Assert.NotNull(receipt2);
 
-    //     Assert.True(receipt1.TransactionHash.Length == 66);
-    //     Assert.True(receipt2.TransactionHash.Length == 66);
+        Assert.True(receipt1.TransactionHash.Length == 66);
+        Assert.True(receipt2.TransactionHash.Length == 66);
 
-    //     Assert.Equal(receipt1.To, receipt2.To);
+        Assert.Equal(receipt1.To, receipt2.To);
 
-    //     Assert.Equal(nonce1, 1);
-    //     Assert.Equal(nonce2, 1);
-    // }
+        Assert.Equal(nonce1, 1);
+        Assert.Equal(nonce2, 1);
+    }
+
+    [Fact(Timeout = 120000)]
+    public async Task MultiChainTransaction_ZkToNonZk_Success()
+    {
+        var chainId1 = 300;
+        var chainId2 = 421614;
+
+        var smartWallet = await SmartWallet.Create(personalWallet: await PrivateKeyWallet.Generate(this.Client), chainId: chainId1, gasless: true);
+
+        var randomAddy = await (await PrivateKeyWallet.Generate(this.Client)).GetAddress();
+
+        var receipt1 = await smartWallet.ExecuteTransaction(new ThirdwebTransactionInput(chainId1) { To = randomAddy, });
+        var nonce1 = await smartWallet.GetTransactionCount(chainId: chainId1, blocktag: "latest");
+        var address1 = await smartWallet.GetAddress();
+
+        var receipt2 = await smartWallet.ExecuteTransaction(new ThirdwebTransactionInput(chainId2) { To = randomAddy, });
+        var nonce2 = await smartWallet.GetTransactionCount(chainId: chainId2, blocktag: "latest");
+        var address2 = await smartWallet.GetAddress();
+
+        Assert.NotNull(address1);
+        Assert.NotNull(address2);
+        Assert.NotEqual(address1, address2);
+
+        Assert.NotNull(receipt1);
+        Assert.NotNull(receipt2);
+
+        Assert.True(receipt1.TransactionHash.Length == 66);
+        Assert.True(receipt2.TransactionHash.Length == 66);
+
+        Assert.NotEqual(receipt1.To, receipt2.To);
+
+        Assert.Equal(nonce1, 1);
+        Assert.Equal(nonce2, 1);
+    }
+
+    [Fact(Timeout = 120000)]
+    public async Task MultiChainTransaction_NonZkToZk_Success()
+    {
+        var chainId1 = 421614;
+        var chainId2 = 300;
+
+        var smartWallet = await SmartWallet.Create(personalWallet: await PrivateKeyWallet.Generate(this.Client), chainId: chainId1, gasless: true);
+
+        var randomAddy = await (await PrivateKeyWallet.Generate(this.Client)).GetAddress();
+
+        var receipt1 = await smartWallet.ExecuteTransaction(new ThirdwebTransactionInput(chainId1) { To = randomAddy, });
+        var nonce1 = await smartWallet.GetTransactionCount(chainId: chainId1, blocktag: "latest");
+        var address1 = await smartWallet.GetAddress();
+
+        var receipt2 = await smartWallet.ExecuteTransaction(new ThirdwebTransactionInput(chainId2) { To = randomAddy, });
+        var nonce2 = await smartWallet.GetTransactionCount(chainId: chainId2, blocktag: "latest");
+        var address2 = await smartWallet.GetAddress();
+
+        Assert.NotNull(address1);
+        Assert.NotNull(address2);
+        Assert.NotEqual(address1, address2);
+
+        Assert.NotNull(receipt1);
+        Assert.NotNull(receipt2);
+
+        Assert.True(receipt1.TransactionHash.Length == 66);
+        Assert.True(receipt2.TransactionHash.Length == 66);
+
+        Assert.NotEqual(receipt1.To, receipt2.To);
+
+        Assert.Equal(nonce1, 1);
+        Assert.Equal(nonce2, 1);
+    }
 }
