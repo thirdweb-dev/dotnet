bee15186297a0c60d8d2608037634aa4e4ceb9a1
0xFirekeeper <0xFirekeeper@gmail.com>
2025-02-08 01:30:01 +0700

    make tracking utils public

--- Thirdweb/Thirdweb.Utils/Utils.cs
+++ Thirdweb/Thirdweb.Utils/Utils.cs
@@ -1192,7 +1192,7 @@ public static partial class Utils
         return value.ToBytesForRLPEncoding();
     }
 
-    internal static async void TrackTransaction(ThirdwebTransaction transaction, string transactionHash)
+    public static async void TrackTransaction(ThirdwebTransaction transaction, string transactionHash)
     {
         try
         {
@@ -1223,7 +1223,7 @@ public static partial class Utils
         }
     }
 
-    internal static async void TrackConnection(IThirdwebWallet wallet)
+    public static async void TrackConnection(IThirdwebWallet wallet)
     {
         try
         {
