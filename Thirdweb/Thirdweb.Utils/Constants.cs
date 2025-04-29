a668d2c2f5ac2816aa2f40e3aec390c4d42aec97
0xFirekeeper <0xFirekeeper@gmail.com>
2025-04-29 20:53:45 +0700

    ver

--- Thirdweb/Thirdweb.Utils/Constants.cs
+++ Thirdweb/Thirdweb.Utils/Constants.cs
@@ -2,7 +2,7 @@
 
 public static class Constants
 {
-    public const string VERSION = "2.19.1";
+    public const string VERSION = "2.20.0";
 
     internal const string BRIDGE_API_URL = "https://bridge.thirdweb.com";
     internal const string NEBULA_API_URL = "https://nebula-api.thirdweb.com";
