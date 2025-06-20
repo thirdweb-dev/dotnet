91119f799eab61469b8068bb5e6f46a7eeeb0296
0xFirekeeper <0xFirekeeper@gmail.com>
2025-06-21 03:44:21 +0700

    InAppWallet EIP7702 Session Key Integration

--- Thirdweb.Console/Program.cs
+++ Thirdweb.Console/Program.cs
@@ -360,48 +360,48 @@ var privateKeyWallet = await PrivateKeyWallet.Generate(client);
 
 #region EIP-7702
 
-// var chain = 11155111; // sepolia
+var chain = 11155111; // sepolia
 
-// // Connect to EOA
-// var smartEoa = await InAppWallet.Create(client, authProvider: AuthProvider.Google, executionMode: ExecutionMode.EIP7702Sponsored);
-// if (!await smartEoa.IsConnected())
-// {
-//     _ = await smartEoa.LoginWithOauth(
-//         isMobile: false,
-//         (url) =>
-//         {
-//             var psi = new ProcessStartInfo { FileName = url, UseShellExecute = true };
-//             _ = Process.Start(psi);
-//         }
-//     );
-// }
-// var smartEoaAddress = await smartEoa.GetAddress();
-// Console.WriteLine($"User Wallet address: {await smartEoa.GetAddress()}");
+// Connect to EOA
+var smartEoa = await InAppWallet.Create(client, authProvider: AuthProvider.Google, executionMode: ExecutionMode.EIP7702Sponsored);
+if (!await smartEoa.IsConnected())
+{
+    _ = await smartEoa.LoginWithOauth(
+        isMobile: false,
+        (url) =>
+        {
+            var psi = new ProcessStartInfo { FileName = url, UseShellExecute = true };
+            _ = Process.Start(psi);
+        }
+    );
+}
+var smartEoaAddress = await smartEoa.GetAddress();
+Console.WriteLine($"User Wallet address: {await smartEoa.GetAddress()}");
 
-// // Upgrade EOA - This wallet explicitly uses EIP-7702 delegation to the thirdweb MinimalAccount (will delegate upon first tx)
+// Upgrade EOA - This wallet explicitly uses EIP-7702 delegation to the thirdweb MinimalAccount (will delegate upon first tx)
 
-// // Transact, will upgrade EOA
-// var receipt = await smartEoa.Transfer(chainId: chain, toAddress: await Utils.GetAddressFromENS(client, "vitalik.eth"), weiAmount: 0);
-// Console.WriteLine($"Transfer Receipt: {receipt.TransactionHash}");
+// Transact, will upgrade EOA
+var receipt = await smartEoa.Transfer(chainId: chain, toAddress: await Utils.GetAddressFromENS(client, "vitalik.eth"), weiAmount: 0);
+Console.WriteLine($"Transfer Receipt: {receipt.TransactionHash}");
 
-// // Double check that it was upgraded
-// var isDelegated = await Utils.IsDelegatedAccount(client, chain, smartEoaAddress);
-// Console.WriteLine($"Is delegated: {isDelegated}");
+// Double check that it was upgraded
+var isDelegated = await Utils.IsDelegatedAccount(client, chain, smartEoaAddress);
+Console.WriteLine($"Is delegated: {isDelegated}");
 
-// // Create a session key
-// var sessionKeyReceipt = await smartEoa.CreateSessionKey(
-//     chain,
-//     new SessionSpec()
-//     {
-//         Signer = await Utils.GetAddressFromENS(client, "0xfirekeeper.eth"),
-//         IsWildcard = true,
-//         ExpiresAt = Utils.GetUnixTimeStampNow() + 86400, // 1 day
-//         CallPolicies = new List<CallSpec>(),
-//         TransferPolicies = new List<TransferSpec>(),
-//         Uid = Guid.NewGuid().ToByteArray().PadTo32Bytes()
-//     }
-// );
-// Console.WriteLine($"Session key receipt: {sessionKeyReceipt.TransactionHash}");
+// Create a session key
+var sessionKeyReceipt = await smartEoa.CreateSessionKey(
+    chain,
+    new SessionSpec()
+    {
+        Signer = await Utils.GetAddressFromENS(client, "vitalik.eth"),
+        IsWildcard = true,
+        ExpiresAt = Utils.GetUnixTimeStampNow() + 86400, // 1 day
+        CallPolicies = new List<CallSpec>(),
+        TransferPolicies = new List<TransferSpec>(),
+        Uid = "my-session-key-uid".HashMessage().HexToBytes()
+    }
+);
+Console.WriteLine($"Session key receipt: {sessionKeyReceipt.TransactionHash}");
 
 #endregion
 
--- Thirdweb/Thirdweb.Wallets/InAppWallet/EcosystemWallet/EcosystemWallet.cs
+++ Thirdweb/Thirdweb.Wallets/InAppWallet/EcosystemWallet/EcosystemWallet.cs
@@ -450,19 +450,23 @@ public partial class EcosystemWallet : IThirdwebWallet
         return $"{redirectUrl}{queryString}";
     }
 
-    public Task<ThirdwebTransactionReceipt> CreateSessionKey(BigInteger chainId, SessionSpec sessionKeyParams)
+    public async Task<ThirdwebTransactionReceipt> CreateSessionKey(BigInteger chainId, SessionSpec sessionKeyParams)
     {
-        throw new NotImplementedException("CreateSessionKey via EIP7702 execution modes is not implemented yet, check back in later versions.");
-        // if (this.ExecutionMode is not ExecutionMode.EIP7702 and not ExecutionMode.EIP7702Sponsored)
-        // {
-        //     throw new InvalidOperationException("CreateSessionKey is only supported for EIP7702 and EIP7702Sponsored execution modes.");
-        // }
+        if (this.ExecutionMode is not ExecutionMode.EIP7702 and not ExecutionMode.EIP7702Sponsored)
+        {
+            throw new InvalidOperationException("CreateSessionKey is only supported for EIP7702 and EIP7702Sponsored execution modes.");
+        }
 
-        // var userWalletAddress = await this.GetAddress();
-        // var sessionKeySig = await EIP712.GenerateSignature_SmartAccount_7702("MinimalAccount", "1", chainId, userWalletAddress, sessionKeyParams, this);
-        // var userContract = await ThirdwebContract.Create(this.Client, userWalletAddress, chainId, Constants.MINIMAL_ACCOUNT_7702_ABI);
-        // var sessionKeyCallData = userContract.CreateCallData("createSessionWithSig", sessionKeyParams, sessionKeySig.HexToBytes());
-        // return await this.ExecuteTransaction(new ThirdwebTransactionInput(chainId: chainId, to: userWalletAddress, value: 0, data: sessionKeyCallData));
+        var userWalletAddress = await this.GetAddress();
+        var sessionKeySig = await EIP712.GenerateSignature_SmartAccount_7702("MinimalAccount", "1", chainId, userWalletAddress, sessionKeyParams, this);
+        var userContract = await ThirdwebContract.Create(this.Client, userWalletAddress, chainId, Constants.MINIMAL_ACCOUNT_7702_ABI);
+        var sessionKeyCallData = userContract.CreateCallData("createSessionWithSig", sessionKeyParams, sessionKeySig.HexToBytes());
+
+        var tx = await ThirdwebTransaction.Create(this, new ThirdwebTransactionInput(chainId: chainId, to: userWalletAddress, value: 0, data: sessionKeyCallData));
+        var result = await ThirdwebTransaction.Simulate(tx).ConfigureAwait(false);
+        Console.WriteLine($"Simulated session key creation transaction: {result}");
+
+        return await this.ExecuteTransaction(new ThirdwebTransactionInput(chainId: chainId, to: userWalletAddress, value: 0, data: sessionKeyCallData));
     }
 
     #endregion
