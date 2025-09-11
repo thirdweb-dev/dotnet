2035299f0d2e753832ff629eb04c3afbdaf21c67
0xFirekeeper <0xFirekeeper@gmail.com>
2025-09-12 05:44:44 +0700

    v2.25.2

--- Thirdweb/Thirdweb.Utils/Constants.cs
+++ Thirdweb/Thirdweb.Utils/Constants.cs
@@ -2,7 +2,7 @@
 
 public static class Constants
 {
-    public const string VERSION = "2.25.1";
+    public const string VERSION = "2.25.2";
 
     internal const string BRIDGE_API_URL = "https://bridge.thirdweb.com";
     internal const string INSIGHT_API_URL = "https://insight.thirdweb.com";
